f3b0c62ccfd6eba2a61ccdfd8f4fde3ffa176c82
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-09-29 16:20:34 +0000

    Don't launch the game if the compiler bails

--- make.sh
+++ make.sh
@@ -10,8 +10,14 @@ g++ -g3 -od src/game.cpp -o build/game \
 -L/home/scallywag/game/external/glfw-3.1.2/include/GLFW/ \
 -L/home/scallywag/game/external/glm-0.9.7.1/ \
 -L/home/scallywag/game/common/ \
--L/home/scallywag/game/src/ \
+-L/home/scallywag/game/src/
 
-cd build/
+# Successful build, launch the thing!
+if [ $? -eq 0 ]; then
 
-./game >> /dev/tty
+  cd build/
+
+  ./game >> /dev/tty
+
+fi
+
