d34f191c276cb3f00c496d4a329d94effdf14625
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-11-01 12:05:48 +0000

    Remove unnecessary linker flags

--- make.sh
+++ make.sh
@@ -6,11 +6,6 @@ g++ -g3 -od src/game.cpp -o build/game \
 -I/home/scallywag/game/external/glm-0.9.7.1/ \
 -I/home/scallywag/game/common/ \
 -I/home/scallywag/game/src/ \
-\
--L/home/scallywag/game/external/glfw-3.1.2/include/GLFW/ \
--L/home/scallywag/game/external/glm-0.9.7.1/ \
--L/home/scallywag/game/common/ \
--L/home/scallywag/game/src/
 
 # Successful build, launch the thing!
 if [ $? -eq 0 ]; then
