45b6c405005da5101fee82c1a4bac58020fa90ef
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-03-17 19:15:00 +0000

    Move make.sh to actually use make

--- make.sh
+++ make.sh
@@ -1,69 +1,9 @@
 #! /bin/bash
 
+make
 
-if [ "$WIN32" == "1" ]; then
-
-  Source="src/game.cpp"
-
-  IncludeDirectories="
-    /I external/glm-0.9.7.1/
-    /I src
-    /I src/GLFW
-    /I src/GL
-  "
-
-  cl /MD /EHsc                  \
-      /D_WIN32=1                \
-      /DGLEW_BUILD=GLEW_STATIC  \
-      $Source                   \
-      $Optimizations            \
-      $IncludeDirectories       \
-      lib/glfw3.lib             \
-      Gdi32.lib                 \
-      User32.lib                \
-      Shell32.lib               \
-      Opengl32.lib
-
-  cp game.exe build/bonsai.exe
-  rm game.*
-
-else
-
-  Source="src/game.cpp -o build/bonsai"
-
-  WarningLevel="-Wall"
-
-  Optimizations=""
-  # Optimizations="-Ofast"
-
-  DebuggerSymbols="-ggdb"
-
-  LinkerFlags="-lglfw -lGLEW -lGLU -lGL"
-
-  IncludeDirectories="
-  -I./external/glm-0.9.7.1/
-  -I./common/
-  -I./src/GLFW/
-  -I./src/
-  "
-
-  g++ -DLINUX=1         \
-      $Optimizations    \
-      $WarningLevel     \
-      $DebuggerSymbols  \
-      $Source           \
-      $LinkerFlags      \
-      $IncludeDirectories
-
-
-fi
-
-# Successful build, launch the thing!
 if [ $? -eq 0 ]; then
-
   cd build/
-
-  ./bonsai >> /dev/tty
-
+  ./Bonsai >> /dev/tty
 fi
 
