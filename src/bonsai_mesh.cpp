10aef8292e050b226308bb8f512d6d6c7372265e
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-12-01 13:38:58 -0800

    SIMD BufferVertsDirect

--- src/bonsai_mesh.cpp
+++ src/bonsai_mesh.cpp
@@ -1,6 +1,9 @@
 #ifndef BONSAI_MESH_CPP
 #define BONSAI_MESH_CPP
 
+#include <immintrin.h>
+#include <xmmintrin.h>
+
 #include <colors.h>
 
 void
@@ -8,7 +11,7 @@ RenderGBuffer(mesh_buffer_target *Target, g_buffer_render_group *gBuffer, shadow
 
 inline void
 BufferVertsDirect(
-    mesh_buffer_target *Target,
+    mesh_buffer_target *Dest,
 
     s32 NumVerts,
 
@@ -22,31 +25,108 @@ BufferVertsDirect(
   TIMED_FUNCTION();
 
   // This path assumes we've already checked there's enough memroy remaining
-  if ( Target->VertsFilled + NumVerts > Target->VertsAllocated )
+  if ( Dest->VertsFilled + NumVerts > Dest->VertsAllocated )
   {
-    Warn("Ran out of memory pushing %d Verts onto Mesh with %d/%d used", NumVerts, Target->VertsFilled, Target->VertsAllocated);
+    Warn("Ran out of memory pushing %d Verts onto Mesh with %d/%d used", NumVerts, Dest->VertsFilled, Dest->VertsAllocated);
     return;
   }
 
+#define SIMD_BUFFER_VERTS 1
+#if SIMD_BUFFER_VERTS
+  __m128 mmScale = _mm_set_ps(0, Scale.z, Scale.y, Scale.x);
+  __m128 mmOffset = _mm_set_ps(0, Offset.z, Offset.y, Offset.x);
 
-  // FIXME(Jesse): This is insanely slow .. how do we fix it?
+  s32 FaceVerts = 6;
+  Assert(NumVerts % FaceVerts == 0);
+
+  for ( s32 VertIndex = 0;
+        VertIndex < NumVerts;
+        VertIndex += FaceVerts )
+  {
+    v3 VertSrc0 = VertsPositions[VertIndex + 0];
+    v3 VertSrc1 = VertsPositions[VertIndex + 1];
+    v3 VertSrc2 = VertsPositions[VertIndex + 2];
+    v3 VertSrc3 = VertsPositions[VertIndex + 3];
+    v3 VertSrc4 = VertsPositions[VertIndex + 4];
+    v3 VertSrc5 = VertsPositions[VertIndex + 5];
+
+    f32_reg Vert0;
+    f32_reg Vert1;
+    f32_reg Vert2;
+    f32_reg Vert3;
+    f32_reg Vert4;
+    f32_reg Vert5;
+
+    Vert0.Sse = _mm_set_ps(0, VertSrc0.z, VertSrc0.y, VertSrc0.x);
+    Vert1.Sse = _mm_set_ps(0, VertSrc1.z, VertSrc1.y, VertSrc1.x);
+    Vert2.Sse = _mm_set_ps(0, VertSrc2.z, VertSrc2.y, VertSrc2.x);
+    Vert3.Sse = _mm_set_ps(0, VertSrc3.z, VertSrc3.y, VertSrc3.x);
+    Vert4.Sse = _mm_set_ps(0, VertSrc4.z, VertSrc4.y, VertSrc4.x);
+    Vert5.Sse = _mm_set_ps(0, VertSrc5.z, VertSrc5.y, VertSrc5.x);
+
+    Vert0.Sse = _mm_add_ps( _mm_mul_ps(Vert0.Sse, mmScale), mmOffset);
+    Vert1.Sse = _mm_add_ps( _mm_mul_ps(Vert1.Sse, mmScale), mmOffset);
+    Vert2.Sse = _mm_add_ps( _mm_mul_ps(Vert2.Sse, mmScale), mmOffset);
+    Vert3.Sse = _mm_add_ps( _mm_mul_ps(Vert3.Sse, mmScale), mmOffset);
+    Vert4.Sse = _mm_add_ps( _mm_mul_ps(Vert4.Sse, mmScale), mmOffset);
+    Vert5.Sse = _mm_add_ps( _mm_mul_ps(Vert5.Sse, mmScale), mmOffset);
+
+    v3 Result0 = {{ Vert0.F[0], Vert0.F[1], Vert0.F[2] }};
+    v3 Result1 = {{ Vert1.F[0], Vert1.F[1], Vert1.F[2] }};
+    v3 Result2 = {{ Vert2.F[0], Vert2.F[1], Vert2.F[2] }};
+    v3 Result3 = {{ Vert3.F[0], Vert3.F[1], Vert3.F[2] }};
+    v3 Result4 = {{ Vert4.F[0], Vert4.F[1], Vert4.F[2] }};
+    v3 Result5 = {{ Vert5.F[0], Vert5.F[1], Vert5.F[2] }};
+
+    Dest->VertexData[Dest->VertsFilled + 0] = Result0;
+    Dest->VertexData[Dest->VertsFilled + 1] = Result1;
+    Dest->VertexData[Dest->VertsFilled + 2] = Result2;
+    Dest->VertexData[Dest->VertsFilled + 3] = Result3;
+    Dest->VertexData[Dest->VertsFilled + 4] = Result4;
+    Dest->VertexData[Dest->VertsFilled + 5] = Result5;
+
+    Dest->NormalData[Dest->VertsFilled + 0] = Normals[VertIndex + 0];
+    Dest->NormalData[Dest->VertsFilled + 1] = Normals[VertIndex + 1];
+    Dest->NormalData[Dest->VertsFilled + 2] = Normals[VertIndex + 2];
+    Dest->NormalData[Dest->VertsFilled + 3] = Normals[VertIndex + 3];
+    Dest->NormalData[Dest->VertsFilled + 4] = Normals[VertIndex + 4];
+    Dest->NormalData[Dest->VertsFilled + 5] = Normals[VertIndex + 5];
+
+    Dest->ColorData[Dest->VertsFilled + 0] = VertColors[VertIndex + 0];
+    Dest->ColorData[Dest->VertsFilled + 1] = VertColors[VertIndex + 1];
+    Dest->ColorData[Dest->VertsFilled + 2] = VertColors[VertIndex + 2];
+    Dest->ColorData[Dest->VertsFilled + 3] = VertColors[VertIndex + 3];
+    Dest->ColorData[Dest->VertsFilled + 4] = VertColors[VertIndex + 4];
+    Dest->ColorData[Dest->VertsFilled + 5] = VertColors[VertIndex + 5];
+
+    Dest->VertsFilled += FaceVerts;
+  }
+
+#else
+
 #if 1
   for ( s32 VertIndex = 0;
         VertIndex < NumVerts;
-        ++ VertIndex)
+        ++VertIndex )
   {
-    Target->VertexData[Target->VertsFilled] = VertsPositions[VertIndex]*Scale + Offset;
-    Target->NormalData[Target->VertsFilled] = Normals[VertIndex];
-    Target->ColorData[Target->VertsFilled] = VertColors[VertIndex];
-    ++Target->VertsFilled;
+    Dest->VertexData[Dest->VertsFilled] = VertsPositions[VertIndex]*Scale + Offset;
+    Dest->NormalData[Dest->VertsFilled] = Normals[VertIndex];
+    Dest->ColorData[Dest->VertsFilled] = VertColors[VertIndex];
+    ++Dest->VertsFilled;
   }
+
+
 #else
   s32 sizeofData = NumVerts * sizeof(v3);
-  memcpy( &Target->VertexData[Target->VertsFilled],  VertsPositions,  sizeofData );
-  memcpy( &Target->NormalData[Target->VertsFilled],  Normals,         sizeofData );
-  memcpy( &Target->ColorData[Target->VertsFilled],   VertColors,      sizeofData );
-  Target->VertsFilled += NumVerts;
+  memcpy( &Dest->VertexData[Dest->VertsFilled],  VertsPositions,  sizeofData );
+  memcpy( &Dest->NormalData[Dest->VertsFilled],  Normals,         sizeofData );
+  memcpy( &Dest->ColorData[Dest->VertsFilled],   VertColors,      sizeofData );
+  Dest->VertsFilled += NumVerts;
 #endif
+
+
+#endif
+
 
   return;
 }
