acec6f21698d9970f9f0e31494a4a5ffde5d310a
Jesse <jesse.hughes.it@gmail.com>
2018-07-20 08:53:37 -0700

    Move chunk-related code into own file

--- src/bonsai.cpp
+++ src/bonsai.cpp
@@ -4,118 +4,7 @@
 #include <shader.cpp>
 #include <texture.cpp> // Only used for font .DDS atm
 #include <render.cpp>
-
-void
-FillChunk(chunk_data *chunk, chunk_dimension Dim, u8 ColorIndex = BLACK)
-{
-  s32 Vol = Volume(Dim);
-
-  for (int i = 0; i < Vol; ++i)
-  {
-    // TODO(Jesse): Pretty sure we don't have to set the faces anymore??
-    SetFlag(&chunk->Voxels[i], (voxel_flag)(Voxel_Filled     |
-                                            Voxel_TopFace    |
-                                            Voxel_BottomFace |
-                                            Voxel_FrontFace  |
-                                            Voxel_BackFace   |
-                                            Voxel_LeftFace   |
-                                            Voxel_RightFace));
-
-
-    chunk->Voxels[i].Color = ColorIndex;
-  }
-
-  SetFlag(chunk, Chunk_Initialized);
-}
-
-
-void
-CopyChunkOffset(world_chunk *Src, voxel_position SrcChunkDim, world_chunk *Dest, voxel_position DestChunkDim, voxel_position Offset)
-{
-  for ( int z = 0; z < DestChunkDim.z; ++ z)
-  {
-    for ( int y = 0; y < DestChunkDim.y; ++ y)
-    {
-      for ( int x = 0; x < DestChunkDim.x; ++ x)
-      {
-        s32 DestIndex = GetIndex(Voxel_Position(x,y,z), DestChunkDim);
-        s32 SynIndex = GetIndex(Voxel_Position(x,y,z) + Offset, SrcChunkDim);
-        Dest->Data->Voxels[DestIndex] = Src->Data->Voxels[SynIndex];
-      }
-    }
-  }
-
-  return;
-}
-
-void
-InitChunkPlane(u32 zIndex, world_chunk *Chunk, chunk_dimension ChunkDim, u8 Color )
-{
-  for ( int z = 0; z < ChunkDim.z; ++ z)
-  {
-    for ( int y = 0; y < ChunkDim.y; ++ y)
-    {
-      for ( int x = 0; x < ChunkDim.x; ++ x)
-      {
-        if (z==zIndex)
-        {
-          s32 Index = GetIndex(Voxel_Position(x,y,z), ChunkDim);
-          Chunk->Data->Voxels[Index].Flags = Voxel_Filled;
-          Chunk->Data->Voxels[Index].Color = Color;
-        }
-      }
-    }
-  }
-
-  return;
-}
-
-
-void
-InitChunkPerlin(perlin_noise *Noise, world_chunk *WorldChunk, chunk_dimension Dim, u8 ColorIndex)
-{
-  Assert(WorldChunk);
-
-  chunk_data *ChunkData = WorldChunk->Data;
-  for ( s32 z = 0; z < Dim.z; ++ z)
-  {
-    for ( s32 y = 0; y < Dim.y; ++ y)
-    {
-      for ( s32 x = 0; x < Dim.x; ++ x)
-      {
-        s32 i = GetIndex(Voxel_Position(x,y,z), Dim);
-        ChunkData->Voxels[i].Flags = Voxel_Uninitialzied;
-
-        Assert( NotSet(&ChunkData->Voxels[i], Voxel_Filled) );
-
-        double InX = ((double)x + ( (double)WORLD_CHUNK_DIM.x*(double)WorldChunk->WorldP.x))/NOISE_FREQUENCY;
-        double InY = ((double)y + ( (double)WORLD_CHUNK_DIM.y*(double)WorldChunk->WorldP.y))/NOISE_FREQUENCY;
-        double InZ = ((double)z + ( (double)WORLD_CHUNK_DIM.z*(double)WorldChunk->WorldP.z))/NOISE_FREQUENCY;
-
-        r32 noiseValue = (r32)Noise->noise(InX, InY, InZ);
-
-        s32 NoiseChoice = Floori(noiseValue + 0.5f);
-
-        Assert(NoiseChoice == 0 || NoiseChoice == 1);
-
-        SetFlag(&ChunkData->Voxels[i], (voxel_flag)(NoiseChoice * Voxel_Filled));
-
-        if (NoiseChoice)
-        {
-          ChunkData->Voxels[i].Color = ColorIndex;
-          Assert( IsSet(&ChunkData->Voxels[i], Voxel_Filled) );
-        }
-        else
-        {
-          Assert( NotSet(&ChunkData->Voxels[i], Voxel_Filled) );
-        }
-
-      }
-    }
-  }
-
-  return;
-}
+#include <chunk.cpp>
 
 void
 PushWorkQueueEntry(work_queue *Queue, work_queue_entry *Entry)
@@ -141,7 +30,6 @@ QueueChunkForInit(game_state *GameState, work_queue *Queue, world_chunk *Chunk)
   Entry.Input = (void*)Chunk;
   Entry.Flags = WorkEntry_InitWorldChunk;
   Entry.GameState = GameState;
-
 
   Chunk->Data->Flags = Chunk_Queued;
 
