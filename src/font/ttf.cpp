a4730c148bbafa26531994858591f91519ba61f0
Jesse <jesse.hughes.it@gmail.com>
2018-09-09 09:26:22 -0700

    Checksumming tables

--- src/font/ttf.cpp
+++ src/font/ttf.cpp
@@ -7,9 +7,23 @@
 #include <unix_platform.cpp>
 
 inline u16
+ReadU16(u8* Source)
+{
+  u32 Result = (Source[0]<<8) + Source[1];
+  return Result;
+}
+
+inline u32
+ReadU32(u8* Source)
+{
+  u32 Result = (Source[0]<<24) + (Source[1]<<16) + (Source[2]<<8) + Source[3];
+  return Result;
+}
+
+inline u16
 ReadU16(binary_stream *Source)
 {
-  u32 Result = (Source->At[0]<<8) + Source->At[1];
+  u32 Result = ReadU16(Source->At);
   Source->At += sizeof(u16);
   return Result;
 }
@@ -17,17 +31,50 @@ ReadU16(binary_stream *Source)
 inline u32
 ReadU32(binary_stream *Source)
 {
-  u32 Result = (Source->At[0]<<24) + (Source->At[1]<<16) + (Source->At[2]<<8) + Source->At[3];
+  u32 Result = ReadU32(Source->At);
   Source->At += sizeof(u32);
   return Result;
 }
 
+
+struct simple_glyph
+{
+  s16 ContourCount;
+  s16 xMin;
+  s16 yMin;
+
+  s16 xMax;
+  s16 yMax;
+
+  u16 EndOfContours;
+
+  u16 InstructionLength;
+  u8 *Instructions; // [InstructionLength]
+
+  u8 *Flags;
+
+  union
+  {
+    u8 xCoord8;
+    u16 xCoord16;
+  };
+  union
+  {
+    u8 yCoord8;
+    u16 yCoord16;
+  };
+};
+
 struct font_table
 {
   u32 Tag;
+  char* HumanTag;
+
   u32 Checksum;
   u32 Offset;
   u32 Length;
+
+  u8* Data;
 };
 
 struct ttf
@@ -52,10 +99,28 @@ struct offset_subtable
   u16 RangeShift;
 };
 
-#define TTF_TAG(s) (s[0]<<24) | (s[1]<<16) | (s[2]<<8) | s[3]
+#define PushArray(Type, Count, Arena) \
+
+
+char *
+HumanTag(u32 Tag, memory_arena *Memory)
+{
+  char* Result = Allocate(char, Memory, 5);
+  char* Bin = (char*)&Tag;
+
+  Result[0] = Bin[3];
+  Result[1] = Bin[2];
+  Result[2] = Bin[1];
+  Result[3] = Bin[0];
+
+  return Result;
+}
+
+#define TTF_TAG(s) \
+  ((s[0]<<24) | (s[1]<<16) | (s[2]<<8) | s[3])
 
 #define AssignTo(prop) \
-  case TTF_TAG(#prop): { Assert(!Font->prop); Font->prop = Table; } break;
+  case TTF_TAG(#prop): { Assert(!Font->prop); Font->prop = Table; Info("Assigned Table : %s", Table->HumanTag);  } break;
 
 inline void
 AssignTable(font_table *Table, ttf *Font)
@@ -74,19 +139,22 @@ AssignTable(font_table *Table, ttf *Font)
 
     default:
     {
-      Info("Unknown Table encountered");
+      Warn("Unknown Table encountered : %s", Table->HumanTag);
     } break;
   }
 }
 
 inline font_table
-ParseFontTable(binary_stream *Source)
+ParseFontTable(binary_stream *Source, memory_arena *Arena)
 {
   font_table Result = {};
   Result.Tag          = ReadU32(Source);
   Result.Checksum     = ReadU32(Source);
   Result.Offset       = ReadU32(Source);
   Result.Length       = ReadU32(Source);
+
+  Result.Data     = Source->Start + Result.Offset;
+  Result.HumanTag = HumanTag(Result.Tag, Arena);
 
   return Result;
 }
@@ -116,16 +184,32 @@ ParseOffsetSubtable(binary_stream *Source)
     } break;
   }
 
-  Result.NumTables    = ReadU16(Source);
-  Result.SearchRange  = ReadU16(Source);
-  Result.EntrySelector= ReadU16(Source);
-  Result.RangeShift   = ReadU16(Source);
+  Result.NumTables     = ReadU16(Source);
+  Result.SearchRange   = ReadU16(Source);
+  Result.EntrySelector = ReadU16(Source);
+  Result.RangeShift    = ReadU16(Source);
 
   return Result;
 }
 
+u32
+CalculateTableChecksum(font_table *Table)
+{
+  u32 Sum = 0;
+  u8* TableData = Table->Data;
+
+  u32 FourByteChunks = (Table->Length + 3) / 4;
+  while (FourByteChunks-- > 0)
+  {
+      Sum += ReadU32(TableData);
+      TableData += 4;
+  }
+
+  return Sum;
+}
+
 ttf
-InitTtf(const char* SourceFile, memory_arena *Arena)
+InitTTF(const char* SourceFile, memory_arena *Arena)
 {
   binary_stream Source = BinaryStreamFromFile(SourceFile, Arena);
 
@@ -136,8 +220,17 @@ InitTtf(const char* SourceFile, memory_arena *Arena)
       TableIndex < TableOffsets.NumTables;
       ++TableIndex)
   {
-    font_table CurrentTable = ParseFontTable(&Source);
-    AssignTable(&CurrentTable, &Result);
+    font_table CurrentTable = ParseFontTable(&Source, Arena);
+
+    u32 Checksum = CalculateTableChecksum(&CurrentTable);
+    if (!Checksum == CurrentTable.Checksum)
+    {
+      Warn("Invalid checksum encountered when reading table %s", CurrentTable.HumanTag);
+    }
+    else
+    {
+      AssignTable(&CurrentTable, &Result);
+    }
   }
 
 
@@ -148,7 +241,7 @@ int
 main()
 {
   memory_arena Arena = {};
-  ttf Font = InitTtf("roboto_for_powerline.ttf", &Arena);
+  ttf Font = InitTTF("roboto_for_powerline.ttf", &Arena);
 
   return 0;
 }
