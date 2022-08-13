fab13c1c90f51cc0793411449caf51369196c663
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-13 14:49:21 -0600

    Change name of metaprogramming folder -> poof

--- scripts/sloc.sh
+++ scripts/sloc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo "Meta output : $(find src/metaprogramming/output -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)"
+echo "Meta output : $(find src/poof/output -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)"
 echo "Total SLOC  : $(find src -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)"
--- src/tests/fixtures/preprocessor/errors/error9.cpp
+++ src/tests/fixtures/preprocessor/errors/error9.cpp
@@ -1,3 +1,3 @@
 #include <src/tests/fixtures/preprocessor/errors/assert_include.h>
 #include <src/tests/fixtures/preprocessor/errors/assert_include_2.h>
-/* #include <src/metaprogramming/output/stream_and_cursor_counted_string.h> */
+/* #include <src/poof/output/stream_and_cursor_counted_string.h> */
