5859416ed54f0c37193a2c2ed7d12061b07f4097
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-13 18:01:07 -0600

    Remove scripts

--- scripts/check_if_terminal_interactive.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-RunningInInteractiveTerminal=0
-
-if [ -t 1 ]; then
- RunningInInteractiveTerminal=1
-fi
--- scripts/set_platform.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-Platform="Unknown"
-UNAME=$(uname)
-if [ "$UNAME" == "Linux" ] ; then
-  Platform="Linux"
-elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
-  Platform="Windows"
-fi
