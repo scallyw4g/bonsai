fd72082cb3145533f959bafe60c80078e6b50bf1
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-26 15:40:25 -0600

    Remove old/dead example code

--- examples/building/game_constants.h
+++
@@ -1,6 +0,0 @@
-
-global_variable chunk_dimension
-g_VisibleRegion = Chunk_Dimension(32, 32, 16);
-
-global_variable chunk_dimension
-WORLD_CHUNK_DIM = Chunk_Dimension(32, 32, 16);
--- examples/space_invaders/vars.cmake
+++
@@ -1,3 +0,0 @@
-
-set( GAME_SRC_DIR ${EXAMPLES}/space_invaders )
-
