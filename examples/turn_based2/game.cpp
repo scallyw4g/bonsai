d88f119e9754f77075529604b1972531361c1c00
a pirate <jesse.hughes.it@gmail.com>
2023-08-12 18:25:01 -0600

    Add turn_based2

---
+++ examples/turn_based2/game.cpp
@@ -0,0 +1,422 @@
+#define PLATFORM_GL_IMPLEMENTATIONS 1
+#define DEBUG_SYSTEM_API 1
+
+#include <bonsai_types.h>
+
+#include <game_constants.h>
+#include <game_types.h>
+
+link_internal u32
+Checkerboard( perlin_noise *Noise,
+              world_chunk *Chunk,
+              chunk_dimension Dim,
+
+              chunk_dimension SrcToDest,
+
+              u8 ColorIndex,
+              s32 Frequency,
+              s32 Amplitude,
+              s64 zMin,
+
+              chunk_dimension WorldChunkDim,
+              void *Ignored )
+{
+  u32 Result = 0;
+
+  auto AbsX = Abs(Chunk->WorldP.x);
+  auto AbsY = Abs(Chunk->WorldP.y);
+  if ( AbsX % 2 == 0 && AbsY % 2 == 1) { ColorIndex = RED; }
+  if ( AbsX % 2 == 1 && AbsY % 2 == 0) { ColorIndex = BLUE; }
+
+  for ( s32 z = 0; z < Dim.z; ++ z)
+  {
+    s64 WorldZ = z - zMin - SrcToDest.z + (WorldChunkDim.z*Chunk->WorldP.z);
+    for ( s32 y = 0; y < Dim.y; ++ y)
+    {
+      for ( s32 x = 0; x < Dim.x; ++ x)
+      {
+        if (WorldZ < zMin)
+        {
+          s32 Index = GetIndex(Voxel_Position(x,y,z), Dim);
+          Chunk->Voxels[Index].Flags = Voxel_Filled;
+          Chunk->Voxels[Index].Color = ColorIndex;
+          ++Result;
+        }
+      }
+    }
+  }
+
+  return Result;;
+}
+
+link_internal u32
+HoodooTerrain( perlin_noise *Noise,
+                      world_chunk *Chunk,
+                      chunk_dimension Dim,
+                      chunk_dimension SrcToDest,
+                      u8 ColorIndex,
+                      s32 Frequency,
+                      s32 Amplitude,
+                      s64 zMin,
+                      chunk_dimension WorldChunkDim,
+                      void *BaseNoise )
+{
+  TIMED_FUNCTION();
+
+  u32 ChunkSum = 0;
+
+  s32 MinZ = Chunk->WorldP.z*WorldChunkDim.z;
+  s32 MaxZ = MinZ+WorldChunkDim.z ;
+
+  octave_buffer *OctaveBuf = (octave_buffer*)BaseNoise;
+  u32 OctaveCount = OctaveBuf->Count;
+
+  for ( s32 z = 0; z < Dim.z; ++ z)
+  {
+    s64 WorldZ = z - SrcToDest.z + (WorldChunkDim.z*Chunk->WorldP.z);
+    s64 WorldZBiased = WorldZ - zMin;
+    for ( s32 y = 0; y < Dim.y; ++ y)
+    {
+      s64 WorldY = y - SrcToDest.y + (WorldChunkDim.y*Chunk->WorldP.y);
+      for ( s32 x = 0; x < Dim.x; ++ x)
+      {
+        s64 WorldX = x - SrcToDest.x + (WorldChunkDim.x*Chunk->WorldP.x);
+        r32 NoiseValue = 0.f;
+        s32 VoxIndex = GetIndex(Voxel_Position(x,y,z), Dim);
+        Chunk->Voxels[VoxIndex].Flags = Voxel_Empty;
+        Assert( NotSet(&Chunk->Voxels[VoxIndex], Voxel_Filled) );
+
+        s32 Amp2 = Amplitude*2;
+        for (u32 OctaveIndex = 0; OctaveIndex < OctaveCount; ++OctaveIndex)
+        {
+          octave *Octave = OctaveBuf->Octaves+OctaveIndex;
+
+          v3 OctaveFrequency = Octave->Freq;
+          r32 OctaveAmplitude = Octave->Amp;
+
+          /* r32 WarpFactor = 3.f; */
+
+          f32 InX = SafeDivide0((x + SrcToDest.x + ( WorldChunkDim.x*Chunk->WorldP.x)), OctaveFrequency.x);
+          f32 InY = SafeDivide0((y + SrcToDest.y + ( WorldChunkDim.y*Chunk->WorldP.y)), OctaveFrequency.y);
+          f32 InZ = SafeDivide0((z + SrcToDest.z + ( WorldChunkDim.z*Chunk->WorldP.z)), OctaveFrequency.z);
+          /* f32 InZ = 1.0; */
+
+
+          r32 Warp = PerlinNoise(InX, InY, InZ);
+          v3 WarpFactor = Octave->WarpStrength*Warp;
+          /* Warp = PerlinNoise(InX, InY, InZ) * r32(1.f/(OctaveIndex+1)); */
+          /* Warp = PerlinNoise(InX, InY, InZ); */
+
+          /* r32 N = PerlinNoise(InX, InY, InZ); */
+          /* r32 N = PerlinNoise(InX, InY+Warp, InZ); */
+          r32 N = PerlinNoise(InX+WarpFactor.x, InY+WarpFactor.y, InZ+WarpFactor.z);
+          /* r32 N = PerlinNoise(Warp, Warp, Warp); */
+
+          /* r32 N2 = PerlinNoise(InX, InY, InZ); */
+
+          Assert(N <= 1.05f);
+          Assert(N > -1.05f);
+
+          /* Assert(N2 <= 1.05f); */
+          /* Assert(N2 > -1.05f); */
+
+          /* b32 NoiseChoice = IsUnderground && NoiseValue > 0.20f; */
+          /* s32 zValue = (s32)Abs( (N*OctaveAmplitude) ); */
+          /* s32 zValue = (s32)Abs( (N*OctaveAmplitude) + (N2*Amp2) ); */
+          /* s32 zValue =  s32(N*OctaveAmplitude) + s32(N2*Amplitude*Amplitude); */
+          /* s32 zValue = s32(N*OctaveAmplitude); */
+
+          NoiseValue += (N*OctaveAmplitude) / 1+OctaveIndex;
+
+          /* if (Sin(InX) > 0.8f) */
+          {
+             octave HoodooOctaves[2] = {
+               {V3(15, 15, 300), 80, V3(1.f)},
+               {V3(50, 50, 4),   20, V3(1.f)},
+             };
+
+
+             RangeIterator(HoodooOctaveIndex, (s32)ArrayCount(HoodooOctaves))
+             {
+               octave HoodooOctave = HoodooOctaves[HoodooOctaveIndex];
+               f32 OctaveX = SafeDivide0((x + SrcToDest.x + ( WorldChunkDim.x*Chunk->WorldP.x)), HoodooOctave.Freq.x);
+               f32 OctaveY = SafeDivide0((y + SrcToDest.y + ( WorldChunkDim.y*Chunk->WorldP.y)), HoodooOctave.Freq.y);
+               f32 OctaveZ = SafeDivide0((z + SrcToDest.z + ( WorldChunkDim.z*Chunk->WorldP.z)), HoodooOctave.Freq.z);
+
+               r32 N2 = PerlinNoise(OctaveX, OctaveY, OctaveZ);
+
+               /* NoiseValue += N2*HoodooOctave.Amp * ((Sin(InX)+Cos(InY))/2.f); */
+               /* NoiseValue += N2*HoodooOctave.Amp * Pow((Sin(InX) + Cos(InY))/20, 3); */
+
+               // Pretty good hoodoos
+               /* NoiseValue += Pow(ClampMin(N2 * Sin(InX)+Cos(InY)-0.95f, 0.f), 3) * HoodooOctave.Amp; */
+
+               // Better Hoodoos
+               /* NoiseValue += Pow(ClampMin(N2 * Sin(InX*17)+Cos(InY*17)-0.9f, 0.f), 4) * HoodooOctave.Amp; */
+               /* NoiseValue += Pow(ClampMin(N2 * ((Sin(InX*17)+Cos(InY*3))-0.9f), 0.f), 4) * HoodooOctave.Amp; */
+
+               // Hoodoos along ridges
+               /* NoiseValue += Pow(ClampMin(N2 * N, 0.f), 4) * HoodooOctave.Amp; */
+
+               NoiseValue += Pow(ClampMin(N2 * N, 0.f), 4) * HoodooOctave.Amp;
+
+             }
+          }
+
+          /* b32 IsUnderground =  zValue < WorldZBiased; */
+          /* b32 NoiseChoice = IsUnderground && N > 0.5f; */
+          /* b32 NoiseChoice = IsUnderground; */
+
+          /* if (NoiseChoice) */
+          {
+            /* NoiseValue += (N*OctaveAmplitude) / OctaveCount; */
+            /* NoiseValue += (N*OctaveAmplitude) / 1+OctaveIndex; */
+          }
+
+          /* OctaveAmplitude = Max(1, OctaveAmplitude/2); */
+          /* OctaveFrequency = Max(1, OctaveFrequency/2); */
+        }
+
+
+
+        /* NoiseValue /= r32(Octaves+Octaves); */
+        /* Assert(NoiseValue <= 1.10f); */
+
+        /* b32 NoiseChoice = NoiseValue > 0.5f;; */
+        /* b32 NoiseChoice = NoiseValue > r32(Amplitude); //0.5f;; */
+        b32 NoiseChoice = r64(NoiseValue) > r64(WorldZBiased);
+
+        u8 ThisColor = ColorIndex;
+
+        s32 SnowThreshold = 100;
+        if (NoiseChoice == True && WorldZ > SnowThreshold)
+        {
+          ThisColor = WHITE;
+        }
+
+        s32 SandThreshold = 3;
+        if (NoiseChoice == True && WorldZ < SandThreshold)
+        {
+          ThisColor = YELLOW;
+        }
+
+        /* s32 WaterThreshold = 0; */
+        /* if (NoiseChoice == False && WorldZ < WaterThreshold) */
+        /* { */
+        /*   NoiseChoice = True; */
+        /*   ThisColor = BLUE; */
+        /* } */
+
+        SetFlag(&Chunk->Voxels[VoxIndex], (voxel_flag)(Voxel_Filled*NoiseChoice));
+        Chunk->Voxels[VoxIndex].Color = ThisColor*u8(NoiseChoice);
+        ChunkSum += NoiseChoice;
+
+        Assert( (Chunk->Voxels[VoxIndex].Flags&VoxelFaceMask) == 0);
+
+        if (NoiseChoice)
+        {
+          Assert( IsSet(&Chunk->Voxels[VoxIndex], Voxel_Filled) );
+        }
+        else
+        {
+          Assert( NotSet(&Chunk->Voxels[VoxIndex], Voxel_Filled) );
+        }
+      }
+    }
+  }
+
+  return ChunkSum;
+}
+
+
+// NOTE(Jesse): This function gets called for each worker thread at engine
+// startup, but not the main thread!
+BONSAI_API_WORKER_THREAD_INIT_CALLBACK()
+{
+  Global_ThreadStates = AllThreads;
+  SetThreadLocal_ThreadIndex(ThreadIndex);
+}
+
+// NOTE(Jesse): This is the worker thread loop.  These are a few default
+// implementations of functions for copying data around in the engine.
+//
+// TODO(Jesse): Make these jobs opt-in, such that game code doesn't have to
+// care about these unless it wants to.  Most of these jobs will never have a
+// custom implementation in games until way into development, if ever.
+//
+BONSAI_API_WORKER_THREAD_CALLBACK()
+{
+  switch (Entry->Type)
+  {
+    // NOTE(Jesse): A noop entry is a bug; InvalidCodePath() crashes the process
+    // in debug mode, and does nothing in release mode (in the hopes we handle
+    // whatever else happens gracefully).
+    case type_work_queue_entry_noop: { InvalidCodePath(); } break;
+
+    case type_work_queue_entry_update_world_region:
+    case type_work_queue_entry_rebuild_mesh:
+    case type_work_queue_entry_init_asset:
+    {
+      NotImplemented;
+    } break;
+
+    case type_work_queue_entry_init_world_chunk:
+     {
+       volatile work_queue_entry_init_world_chunk *Job = SafeAccess(work_queue_entry_init_world_chunk, Entry);
+       world_chunk *Chunk = Job->Chunk;
+
+       if (ChunkIsGarbage(Chunk))
+       {
+       }
+       else
+       {
+
+         {
+           s32 Frequency = 300;
+           s32 Amplititude = 220;
+           s32 StartingZDepth = -100;
+           u32 OctaveCount = 2;
+
+           octave_buffer OctaveBuf = { OctaveCount, {} };
+           OctaveBuf.Octaves = Allocate(octave, Thread->TempMemory, OctaveCount);
+
+           OctaveBuf.Octaves[0] = {V3(350, 150, 50), 50, V3(1.f)};
+           OctaveBuf.Octaves[1] = {V3(100, 150, 35),   20, V3(1.f)};
+           /* OctaveBuf.Octaves[2] = {V3(150, 150, 1500),     400, V3(1.f)}; */
+           /* OctaveBuf.Octaves[2] = {V3(500, 500, 20),  300, V3(1.f)}; */
+           /* OctaveBuf.Octaves[2] = {75, 60, 1}; */
+           /* OctaveBuf.Octaves[3] = {37, 30, 0}; */
+
+
+           /* chunk_init_flags InitFlags = ChunkInitFlag_ComputeStandingSpots; */
+           chunk_init_flags InitFlags = ChunkInitFlag_GenMipMapLODs;
+           /* chunk_init_flags InitFlags = ChunkInitFlag_Noop; */
+           InitializeChunkWithNoise( HoodooTerrain, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, InitFlags, (void*)&OctaveBuf);
+         }
+
+         /* { */
+         /*   // Custom flat noise function that produces a checkerboard */
+         /*   s32 Frequency = 0; */
+         /*   s32 Amplititude = 0; */
+         /*   s32 StartingZDepth = -1; */
+         /*   chunk_init_flags InitFlags = ChunkInitFlag_Noop; */
+         /*   /1* chunk_init_flags InitFlags = ChunkInitFlag_GenMipMapLODs; *1/ */
+         /*   InitializeChunkWithNoise( Checkerboard, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, InitFlags, 0); */
+         /* } */
+
+         /* { */
+         /*   // FBM params */
+         /*   s32 Frequency = 300; */
+         /*   s32 Amplititude = 220; */
+         /*   s32 StartingZDepth = -200; */
+         /*   u32 Octaves = 4; */
+         /*   /1* chunk_init_flags InitFlags = ChunkInitFlag_ComputeStandingSpots; *1/ */
+         /*   chunk_init_flags InitFlags = ChunkInitFlag_Noop; */
+         /*   InitializeChunkWithNoise( Noise_FBM2D, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, InitFlags, (void*)&Octaves); */
+         /* } */
+
+         /* { */
+         /*   // Perlin 2D Params */
+         /*   s32 Frequency = 100; */
+         /*   s32 Amplititude = 25; */
+         /*   s32 StartingZDepth = 0; */
+         /*   InitializeChunkWithNoise( Noise_Perlin2D, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, ChunkInitFlag_Noop, 0); */
+         /* } */
+
+         /* { */
+         /*   // Perlin 3D Params */
+         /*   s32 Frequency = 100; */
+         /*   s32 Amplititude = 25; */
+         /*   s32 StartingZDepth = 0; */
+         /*   InitializeChunkWithNoise( Noise_Perlin3D, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, ChunkInitFlag_Noop, 0); */
+         /* } */
+
+         /* { */
+         /*   // Flat Params */
+         /*   s32 Frequency = 100; */
+         /*   s32 Amplititude = 25; */
+         /*   s32 StartingZDepth = -1; */
+         /*   InitializeChunkWithNoise( Noise_Flat, Thread, Chunk, Chunk->Dim, 0, Frequency, Amplititude, StartingZDepth, ChunkInitFlag_Noop, 0); */
+         /* } */
+
+       }
+
+       FinalizeChunkInitialization(Chunk);
+     } break;
+
+    case type_work_queue_entry_copy_buffer_ref:
+    {
+      work_queue_entry_copy_buffer_ref *CopyJob = SafeAccess(work_queue_entry_copy_buffer_ref, Entry);
+      DoCopyJob(CopyJob, &Thread->EngineResources->MeshFreelist, Thread->PermMemory);
+    } break;
+
+    case type_work_queue_entry_copy_buffer:
+    {
+      volatile work_queue_entry_copy_buffer *CopyJob = SafeAccess(work_queue_entry_copy_buffer, Entry);
+      DoCopyJob(CopyJob, &Thread->EngineResources->MeshFreelist, Thread->PermMemory);
+    } break;
+
+    case type_work_queue_entry_copy_buffer_set:
+    {
+      volatile work_queue_entry_copy_buffer_set *CopySet = SafeAccess(work_queue_entry_copy_buffer_set, Entry);
+      RangeIterator(CopyIndex, (s32)CopySet->Count)
+      {
+        volatile work_queue_entry_copy_buffer *CopyJob = &CopySet->CopyTargets[CopyIndex];
+        DoCopyJob(CopyJob, &Thread->EngineResources->MeshFreelist, Thread->PermMemory);
+      }
+    } break;
+
+    case type_work_queue_entry_sim_particle_system:
+    {
+      work_queue_entry_sim_particle_system *Job = SafeAccess(work_queue_entry_sim_particle_system, Entry);
+      SimulateParticleSystem(Job);
+    } break;
+  }
+}
+
+BONSAI_API_MAIN_THREAD_INIT_CALLBACK()
+{
+  UNPACK_ENGINE_RESOURCES(Resources);
+
+  Global_AssetPrefixPath = CSz("examples/terrain_gen/assets");
+
+  world_position WorldCenter = {};
+  canonical_position CameraTargetP = {};
+
+  StandardCamera(Graphics->Camera, 10000.0f, 1000.0f, CameraTargetP);
+
+  AllocateWorld(World, WorldCenter, WORLD_CHUNK_DIM, g_VisibleRegion);
+
+  World->Flags = WorldFlag_WorldCenterFollowsCameraTarget;
+
+  entity *CameraTarget = GetFreeEntity(EntityTable);
+  SpawnEntity( CameraTarget );
+
+  Resources->CameraTarget = CameraTarget;
+
+  GameState = Allocate(game_state, Resources->Memory, 1);
+  return GameState;
+}
+
+
+BONSAI_API_MAIN_THREAD_CALLBACK()
+{
+  Assert(ThreadLocal_ThreadIndex == 0);
+
+  TIMED_FUNCTION();
+  UNPACK_ENGINE_RESOURCES(Resources);
+
+  /* DrawFrustum(World, Graphics, Camera); */
+
+  f32 dt = Plat->dt;
+  f32 Speed = 80.f;
+
+  v3 Offset = GetCameraRelativeInput(Hotkeys, Camera);
+  Offset.z = 0; // Constrain to XY plane
+
+  if (Input->E.Pressed) { Offset.z += 1.f; }
+  if (Input->Q.Pressed) { Offset.z -= 1.f; }
+
+  Offset = Normalize(Offset);
+  Resources->CameraTarget->P.Offset += Offset * dt * Speed;
+}
---
+++ examples/turn_based2/game_constants.h
@@ -0,0 +1,17 @@
+
+#if 0
+global_variable chunk_dimension
+g_VisibleRegion = Chunk_Dimension(32, 32, 8);
+
+global_variable chunk_dimension
+WORLD_CHUNK_DIM = Chunk_Dimension(32, 32, 8);
+#else
+global_variable chunk_dimension
+/* g_VisibleRegion = Chunk_Dimension(64, 64, 8); */
+g_VisibleRegion = Chunk_Dimension(32, 32, 16);
+/* g_VisibleRegion = Chunk_Dimension(8, 8, 8); */
+
+global_variable chunk_dimension
+WORLD_CHUNK_DIM = Chunk_Dimension(32, 32, 32);
+
+#endif
