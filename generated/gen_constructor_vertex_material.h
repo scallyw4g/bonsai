6446f19afde494b884136bbf9b4fd4452bb0dfa1
a pirate <jesse.hughes.it@gmail.com>
2023-10-27 17:00:11 -0600

    Update stdlib

---
+++ generated/gen_constructor_vertex_material.h
@@ -0,0 +1,11 @@
+link_internal vertex_material
+VertexMaterial( v3 Color , f32 Transparency , f32 Emission  )
+{
+  vertex_material Reuslt = {
+    .Color = Color,
+    .Transparency = Transparency,
+    /* .Emission = Emission */
+  };
+  return Reuslt;
+}
+
