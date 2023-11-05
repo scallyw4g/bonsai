1a8a636eb32e7dfbcf6ac84c093918c5ecb387ea
a pirate <jesse.hughes.it@gmail.com>
2023-11-05 10:02:16 -0600

    Add some generated code

---
+++ generated/buffer_ui_toggle_button_handle.h
@@ -0,0 +1,53 @@
+struct ui_toggle_button_handle_buffer
+{
+  ui_toggle_button_handle *Start;
+  umm Count;
+};
+
+link_internal ui_toggle_button_handle_buffer
+UiToggleButtonHandleBuffer(umm ElementCount, memory_arena* Memory)
+{
+  ui_toggle_button_handle_buffer Result = {};
+
+  if (ElementCount)
+  {
+    Result.Start = Allocate( ui_toggle_button_handle, Memory, ElementCount );
+    Result.Count = ElementCount;
+  }
+  else
+  {
+    Warn("Attempted to allocate ui_toggle_button_handle_buffer of 0 length.");
+  }
+
+  return Result;
+}
+
+link_inline umm
+AtElements(ui_toggle_button_handle_buffer *Buf)
+{
+  umm Result = Buf->Count;
+  return Result;
+}
+
+link_inline umm
+CurrentCount(ui_toggle_button_handle_buffer *Buf)
+{
+  umm Result = Buf->Count;
+  return Result;
+}
+
+link_inline ui_toggle_button_handle *
+GetPtr(ui_toggle_button_handle_buffer *Buf, umm Index)
+{
+  ui_toggle_button_handle *Result = 0;
+  if (Index < Buf->Count) { Result = Buf->Start + Index; }
+  return Result;
+}
+
+link_inline ui_toggle_button_handle *
+Get(ui_toggle_button_handle_buffer *Buf, umm Index)
+{
+  ui_toggle_button_handle *Result = GetPtr(Buf, Index);
+  return Result;
+}
+
