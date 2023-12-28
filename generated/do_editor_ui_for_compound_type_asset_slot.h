7a73cec98f09b25d4bfe213eefb3beb894770b84
a pirate <jesse.hughes.it@gmail.com>
2023-12-28 13:27:50 -0800

    Add some generated code

---
+++ generated/do_editor_ui_for_compound_type_asset_slot.h
@@ -0,0 +1,37 @@
+link_internal void
+DoEditorUi(renderer_2d *Ui, asset_slot *Element, cs Name, EDITOR_UI_FUNCTION_PROTO_DEFAULTS)
+{
+  if (Element)
+  {
+    /* PushTableStart(Ui); */
+    if (ToggleButton(Ui, FSz("v %S", Name), FSz("> %S", Name), umm(Element) ^ umm(Name), EDITOR_UI_FUNCTION_INSTANCE_NAMES))
+    {
+      PushForceUpdateBasis(Ui, V2(20.f, 0.f));
+      /* Padding.x += 20.f; */
+      PushNewRow(Ui);
+      DoEditorUi(Ui, &Element->Index, CSz("u16 Index"), EDITOR_UI_FUNCTION_INSTANCE_NAMES);
+
+
+
+
+      PushNewRow(Ui);
+      DoEditorUi(Ui, &Element->Generation, CSz("u16 Generation"), EDITOR_UI_FUNCTION_INSTANCE_NAMES);
+
+
+
+
+      PushNewRow(Ui);
+      PushForceUpdateBasis(Ui, V2(-20.f, 0.f));
+    }
+    else
+    {
+      PushNewRow(Ui);
+    }
+  }
+  else
+  {
+    PushColumn(Ui, FSz("%S = (null)", Name));
+  }
+  
+}
+
---
+++ generated/maybe_asset_id.h
@@ -0,0 +1,6 @@
+struct maybe_asset_id
+{
+  maybe_tag Tag;
+  asset_id Value;
+};
+
