180d2a2a7615fa0e33f492e1d4df62312a443ef8
a pirate <jesse.hughes.it@gmail.com>
2024-02-29 11:47:54 -0800

    Add generated code, update stdlib

---
+++ generated/anonymous_render_settings_0_3Bm1BxXY.h
@@ -0,0 +1,15 @@
+// src/engine/serdes.cpp:361:0
+
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+E1->Member = E0->Member;
+
