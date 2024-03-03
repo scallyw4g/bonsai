9c9fe5fcb4eeb498ac626824b82665127521f597
a pirate <jesse.hughes.it@gmail.com>
2024-03-03 10:12:45 -0800

    Update sloc.sh

--- sloc.sh
+++ sloc.sh
@@ -3,7 +3,7 @@
     META_SLOC=$(find generated   -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
     EXMP_SLOC=$(find examples    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
     DEPS_SLOC=$(find external    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
-    POOF_SLOC=$(find src         -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+    ENGN_SLOC=$(find src         -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
     SHDR_SLOC=$(find shaders     -type f                                  | xargs cat | wc -l)
 STD_SHDR_SLOC=$(find external/bonsai_stdlib/shaders     -type f           | xargs cat | wc -l)
 
@@ -16,12 +16,12 @@ STD_SHDR_SLOC=$(find external/bonsai_stdlib/shaders     -type f           | xarg
 
 
 echo ""
-echo " Hand written   $POOF_SLOC"
+echo " Engine         $ENGN_SLOC"
 echo " Deps           $DEPS_SLOC"
 echo " Poof'd code    $META_SLOC"
 echo " Examples       $EXMP_SLOC"
 echo " Shaders        $SHDR_SLOC"
 echo " stdlib/shaders $STD_SHDR_SLOC"
 echo "--------------------------"
-echo " Total          $(( POOF_SLOC + SHDR_SLOC + EXMP_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC + STD_SHDR_SLOC )) sloc"
+echo " Total          $(( ENGN_SLOC + SHDR_SLOC + EXMP_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC + STD_SHDR_SLOC )) sloc"
 echo ""
