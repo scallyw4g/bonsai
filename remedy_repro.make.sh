c180303aa96d532c068fce52607b39837f35ffdf
a pirate <jesse.hughes.it@gmail.com>
2024-09-11 07:50:52 -0700

    Cleanup

---
+++ remedy_repro.make.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./make.sh $OPT                                                 \
+  BuildExecutables                                             \
+  BuildSingleExample examples/blank_project                    \
