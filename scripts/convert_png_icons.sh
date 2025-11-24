45e73e4ab9a39ec2782ef497df9a85e04fb6ada0
a pirate <jesse.hughes.it@gmail.com>
2025-11-24 11:13:05 -0800

    Add icon assets

---
+++ scripts/convert_png_icons.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+PNG_IMAGES=$(find assets/icons/png/ | grep -e png$ )
+
+for FILE in $PNG_IMAGES; do
+  echo "Converting $FILE"
+
+  Filename="$(basename -- $FILE .png)"
+  magick $FILE assets/icons/$Filename.bmp
+done
+
