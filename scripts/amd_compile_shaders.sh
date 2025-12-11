d902e5e607678ab2bd49ac7233be05dc2bfe15e3
a pirate <jesse.hughes.it@gmail.com>
2025-12-11 10:52:41 -0800

    Fix amd shader compile script polluting root directory

--- scripts/amd_compile_shaders.sh
+++ scripts/amd_compile_shaders.sh
@@ -4,9 +4,13 @@ GLC=~/bin/rdna_tools/utils/glc.exe
 
 # $GLC -gfxip 1100 -set in.vert.glsl="../../work/bonsai/shaders/DepthRTT.vertexshader"
 
+[ ! -d tmp/ ] && mkdir tmp
 
-SHADERS=$(find shaders/ | grep vert )
-HEADER_CODE="$(cat ./external/bonsai_stdlib/shaders/header.glsl)"
+pushd tmp/
+
+
+SHADERS=$(find ../shaders/ | grep vert )
+HEADER_CODE="$(cat ../external/bonsai_stdlib/shaders/header.glsl)"
 
 for FILE in $SHADERS; do
   echo "Compiling $FILE"
@@ -19,7 +23,7 @@ for FILE in $SHADERS; do
   echo "-----  "
 done
 
-SHADERS=$(find shaders/ | grep frag )
+SHADERS=$(find ../shaders/ | grep frag )
 
 for FILE in $SHADERS; do
   echo "Compiling $FILE"
@@ -33,3 +37,5 @@ for FILE in $SHADERS; do
 done
 
 rm tmp.txt
+
+popd
